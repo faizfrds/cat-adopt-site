f3a0107c9fabac7287d60a6c2240ac31e5bee8d5
faiz <faiz.firdaus.2004@gmail.com>
2023-08-02 23:25:44 +0400

    i

---
+++ app/(site)/page.tsx
@@ -0,0 +1,12 @@
+import Header from "@/components/Header";
+import Showcase from "@/components/Showcase";
+
+const Nav = () => {
+  return ( 
+    <div>
+      <Showcase />
+    </div>
+   );
+}
+ 
+export default Nav;
